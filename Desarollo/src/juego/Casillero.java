68f26d5550de1dd179cfb65964fabb6bd360b6d9
Sebastián Ralowiec <54478300+sebasr1995@users.noreply.github.com>
2019-09-13 15:44:00 -0300

    Update Casillero.java

--- Desarollo/src/juego/Casillero.java
+++ Desarollo/src/juego/Casillero.java
@@ -1,5 +1,42 @@
 package juego;
 
 public class Casillero {
-
+  private int id;
+  private string color;
+  private bool tienePersonaje;
+  private bool tieneObjeto;
+  
+  public int getId(){
+    return id;
+  }
+  
+  public void setId(int id){
+    this.id=id;
+  }
+  
+  public string getColor(){
+    return color;
+  }
+  
+  public void setColor(string color){
+    this.color=color;
+  }
+  
+  public bool getTienePersonaje(){
+    return tienePersonaje;
+  }
+  
+  public void setTienePersonaje(bool tienePersonaje){
+    this.tienePersonaje=tienePersonaje;
+  }
+  
+  public bool getTieneObjeto(){
+    return tieneObjeto;
+  }
+  
+  public void setTieneObjeto(bool tieneObjeto){
+    this.tieneObjeto=tieneObjeto;
+  }
+  
+  
 }
