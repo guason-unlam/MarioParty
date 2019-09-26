5370f924f2f4b8ad768639ff288e433a7284b666
lucasarzola <lucas.arzola@gmail.com>
2019-09-26 16:03:50 -0300

    creado de tests de Sala

--- Desarollo/tests/juego/SalaTest.java
+++ Desarollo/tests/juego/SalaTest.java
@@ -1,5 +1,29 @@
 package juego;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import juego.lobby.Sala;
+import juego.lobby.Usuario;
+
 public class SalaTest {
 
+	@Test
+	public void TestSalaLlena() {
+		Usuario usuarioCreador = new Usuario("prueba", "1234");
+		Usuario usuarioParticipante1 = new Usuario("conectarse1", "12345");
+		Usuario usuarioParticipante2 = new Usuario("conectarse2", "123456");
+//		Sala sala = new Sala("SalaTest", 2, usuarioCreador);
+		Sala sala = usuarioCreador.crearSala();
+		usuarioParticipante1.conectarseALaSala(sala);
+		Assert.assertFalse(usuarioParticipante2.conectarseALaSala(sala)); // compruebo que al querer agregar un tercer
+
+	}
+
+	@Test
+	public void TestCreadorSala() {
+		Usuario usuarioCreador = new Usuario("prueba", "1234");
+		Sala sala = usuarioCreador.crearSala();
+		Assert.assertEquals(usuarioCreador, sala.getUsuarioCreador());
+	}
 }
