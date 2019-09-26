6e968ae51bbb4c2f61790a7c7cc10a9b295d1a01
lucasarzola <lucas.arzola@gmail.com>
2019-09-26 16:03:09 -0300

    paso metodo conectarseASala de sala a usuario y seteo de usuario y contraseña

--- Desarollo/src/juego/lobby/Usuario.java
+++ Desarollo/src/juego/lobby/Usuario.java
@@ -69,29 +69,40 @@ public class Usuario {
 
 	public Sala crearSala() {
 		Scanner lector = new Scanner(System.in);
-		String nombreSala;
-		String password;
-		int cantDeUsrMaximos;
+		String nombreSala = "sala"; // para simplificar ahora le asigno un nombre luego hay que leer de consola
+		String password = "1234";
+		int cantDeUsrMaximos = 2; // mas adelante se podra elegir
 
-		// Voy ingresando los parametros
-		System.out.println("Ingrese el nombre de la sala: ");
-		nombreSala = lector.nextLine();
-		// Voy ingresando la pw, PUEDE SER VACIA
-		System.out.println("Ingrese la contraseña de la sala: ");
-		password = lector.nextLine();
-
-		do {
-			// Por ahora salas entre 2 y 5
-			System.out.println("Ingrese la cantidad maxima de usuarios: ");
-			cantDeUsrMaximos = lector.nextInt();
-		} while (cantDeUsrMaximos < 2 && cantDeUsrMaximos > 5);
-
-		lector.close();
+//		// Voy ingresando los parametros
+//		System.out.println("Ingrese el nombre de la sala: ");
+//		nombreSala = lector.nextLine();
+//		// Voy ingresando la pw, PUEDE SER VACIA
+//		System.out.println("Ingrese la contraseña de la sala: ");
+//		password = lector.nextLine();
+//
+//		do {
+//			// Por ahora salas entre 2 y 5
+//			System.out.println("Ingrese la cantidad maxima de usuarios: ");
+//			cantDeUsrMaximos = lector.nextInt();
+//		} while (cantDeUsrMaximos < 2 && cantDeUsrMaximos > 5);
+//
+//	lector.close();
 		// Creo la sala
 		Sala sala = new Sala(nombreSala, password, cantDeUsrMaximos, this);
 		// Conecto al usuario a la misma
-		sala.conectarseALaSala(this);
+		conectarseALaSala(sala);
 		return sala;
+	}
+
+	public boolean conectarseALaSala(Sala sala) {
+		if (sala.getCapacidadActual() < sala.getCapacidadMaxima()) {
+			this.setSala(sala);
+			sala.setUsuariosActivos(this);
+			sala.setCapacidadActual(sala.getCapacidadActual() + 1);
+			return true;
+		}
+		sala.setSalaLlena(true);
+		return false;
 	}
 
 	public void salirDeSala() {
