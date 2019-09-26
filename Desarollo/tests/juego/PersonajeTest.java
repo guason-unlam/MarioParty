286bb3172f4f9010f9cd57d0b84cbfde751474c7
L0687413 <luciano.cabrera@fluxit.com.ar>
2019-09-26 16:07:27 -0300

    se agrega test personaje

---
+++ Desarollo/tests/juego/PersonajeTest.java
@@ -0,0 +1,64 @@
+package juego;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import juego.personas.Personaje;
+
+public class PersonajeTest {
+
+	@Test
+	public void hayPersonajeDescripcion() {
+		
+		Personaje per = new Personaje("Mario","el numero 1","./src/main/");
+		
+		assertEquals("el numero 1",per.getDescripcion());
+		
+	}
+	
+	@Test
+	public void hayPersonajeNombre() {
+		
+		Personaje per = new Personaje("Mario","el numero 1","./src/main/");
+		
+		assertEquals("el numero 1",per.getDescripcion());
+
+	}
+	
+	@Test
+	public void hayPersonajePath() {
+		
+		Personaje per = new Personaje("Mario","el numero 1","./src/main/");
+		
+		assertEquals("./src/main/",per.getPathSkin());
+	}
+	@Test
+	public void noHayPersonajeNombreFalso() {
+		
+		Personaje per = new Personaje("Mario","el numero 1","./src/main/");
+		
+		assertNotEquals("Wario",per.getNombre());
+	}
+	
+	@Test
+	public void noHayPersonajeDescripcionFalsa() {
+		
+		Personaje per = new Personaje("Mario","el numero 1","./src/main/");
+		
+		assertNotEquals("el numero 2",per.getDescripcion());
+		
+	}
+	
+	@Test
+	public void noHayPersonajePathFalso() {
+		
+		Personaje per = new Personaje("Mario","el numero 1","./src/main/");
+		
+		assertNotEquals("./src/mein/",per.getPathSkin());
+	
+	}
+
+}
