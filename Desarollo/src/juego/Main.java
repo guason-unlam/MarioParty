dc206ac921b88540c7800ccfffd97787a81afeb7
Joaquín Krasuk <jkrasuk@alumno.unlam.edu.ar>
2019-10-04 00:27:13 -0300

    Revert "Comienzo con parte visual"
    This reverts commit 0cca495d7db2b5211213285e9712dbd64347b889.

--- Desarollo/src/juego/Main.java
+++ Desarollo/src/juego/Main.java
@@ -4,12 +4,9 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
-import juego.ventana.VentanaLoginUsuario;
-
 public class Main {
 	public static void main(String[] args) {
 		iniciarJuego();
-		VentanaLoginUsuario n = new VentanaLoginUsuario();
 	}
 
 	public static void iniciarJuego() {
